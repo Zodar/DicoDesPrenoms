add0d920939720b41c42c6ed0855f43ff55e9cbd
zodar <zodar@live.fr>
2016-12-27 00:40:04 +0100

    first commit

---
+++ app/src/main/java/tarik/merabet/dicodesprenoms/MainActivity.java
@@ -0,0 +1,27 @@
+package tarik.merabet.dicodesprenoms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        List<String> spinnerArray =  new ArrayList<>();
+        spinnerArray.add("item1");
+        spinnerArray.add("item2");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner = (Spinner) findViewById(R.id.list_origines);
+        spinner.setAdapter(adapter);
+    }
+}
